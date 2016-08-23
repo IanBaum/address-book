46b2ec0bb603ba6883f2324f0529763c4bf645da
Ian Baum and Peter Armington <peter@>
2016-08-23 08:31:33 -0400

    add javascript functions to create address

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
 
+function Address(street, city, state){
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 // user interface logic
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -11,13 +17,23 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+
+    newContact.addresses.push(newAddress);
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
